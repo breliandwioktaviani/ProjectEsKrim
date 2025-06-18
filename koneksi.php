6ceaefc1cd3f0a1eb5b51f699577534b237213b0
breliandwioktaviani <brelianoktaviani@gmail.com>
2025-06-18 09:46:05 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,10 @@
 <?php
 class Database {
-    private $host = "sql301.infinityfree.com";
-    private $username = "if0_39243846";      
-    private $password = "JMXYJkynjY4JhG";
-    private $dbname = "if0_39243846_db_eskrim";
-    private $port = 3306;
+    private $host = namehost ;
+    private $username = username ;      
+    private $password = password ;
+    private $dbname = dbname ;
+    private $port = port ;
     private $conn;
 
     public function connection() {
